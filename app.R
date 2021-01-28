9e5e87cdc0b47b76f6e8c154f8e0f27511d1dc63
Vlad Munteanu <paulvlad34@gmail.com>
2021-01-28 20:23:13 +0200

    Update app.R

--- app.R
+++ app.R
@@ -14,165 +14,6 @@ numeElevi=list('Munteanu Vlad Paul','Alexandru Oana','Mihai Catalin','Alex')
 
 #cod javascript 
 jsCode <- '
-var xVf = [];
-var yVf = [];
-var pct = [];
-var reps = 50000;
-
-function setup() {
-  createCanvas(600, 600);
-  
-  angleMode(DEGREES);
-  
-  pct = [width/2, height/2];
-}
-
-function shp1(k,r,s){
-  let step = 360 / k;
-  let angle = 90;
-  
-  background(0);
-  fill(0,255,0);
-  
-  for(let i = 1; i<=k;i++){
-    let x = width/2 + (r * cos(angle));
-    let y = height/2 - (r * sin(angle));
-    
-    xVf[i] = x;
-    yVf[i] = y;
-    
-    ellipse(x,y,16,16);
-    angle += step;
-  }
-  
-  fill(255);
-  stroke(255);
-  for(let i = 1; i <= reps; i++){
-    point(pct[0],pct[1]);
-    
-    let rVf = floor(random(1,k+1));
-    
-    pct[0] = ((s-1)*pct[0]+xVf[rVf])/s;
-    pct[1] = ((s-1)*pct[1]+yVf[rVf])/s;
-  }
-}
-
-function shp2(k,r,s){
-  let step = 360 / k;
-  let angle = 90;
-  
-  background(0);
-  fill(0,255,0);
-  
-  for(let i = 1; i<=k;i++){
-    let x = width/2 + (r * cos(angle));
-    let y = height/2 - (r * sin(angle));
-    
-    xVf[i] = x;
-    yVf[i] = y;
-    
-    ellipse(x,y,16,16);
-    angle += step;
-  }
-  
-  fill(255);
-  stroke(255);
-  let prevVf = 0;
-  for(let i = 1; i <= reps; i++){
-    //ellipse(pct[0],pct[1], 2, 2);
-    point(pct[0], pct[1]);
-    
-    let rVf = floor(random(1,k+1));
-    while(rVf == prevVf){
-      rVf = floor(random(1,k+1));
-    }
-    prevVf = rVf;
-    
-    pct[0] = ((s-1)*pct[0]+xVf[rVf])/s;
-    pct[1] = ((s-1)*pct[1]+yVf[rVf])/s;
-  }
-}
-
-function shp3(k,r,s){
-  let step = 360 / k;
-  let angle = 90;
-  
-  background(0);
-  fill(0,255,0);
-  
-  for(let i = 1; i<=k;i++){
-    let x = width/2 + (r * cos(angle));
-    let y = height/2 - (r * sin(angle));
-    
-    xVf[i] = x;
-    yVf[i] = y;
-    
-    ellipse(x,y,16,16);
-    angle += step;
-  }
-  
-  fill(255);
-  stroke(255);
-  let prevVf = 0;
-  for(let i = 1; i <= reps; i++){
-    //ellipse(pct[0],pct[1], 2, 2);
-    point(pct[0], pct[1]);
-    
-    let rVf = floor(random(1,k+1));
-    
-    if(prevVf-1 == 0) leftVf = k;
-    else leftVf = prevVf-1;
-    
-    while(rVf == leftVf){
-      rVf = floor(random(1,k+1));
-    }
-    prevVf = rVf;
-    
-    pct[0] = ((s-1)*pct[0]+xVf[rVf])/s;
-    pct[1] = ((s-1)*pct[1]+yVf[rVf])/s;
-  }
-}
-
-function shp4(k,r,s){
-  let step = 360 / k;
-  let angle = 90;
-  
-  background(0);
-  fill(0,255,0);
-  
-  for(let i = 1; i<=k;i++){
-    let x = width/2 + (r * cos(angle));
-    let y = height/2 - (r * sin(angle));
-    
-    xVf[i] = x;
-    yVf[i] = y;
-    
-    ellipse(x,y,16,16);
-    angle += step;
-  }
-  
-  fill(255);
-  stroke(255);
-  let prevVf = 0;
-  for(let i = 1; i <= reps; i++){
-    //ellipse(pct[0],pct[1], 2, 2);
-    point(pct[0], pct[1]);
-    
-    let rVf = floor(random(1,k+1));
-    
-    if(prevVf-2 == 0) leftVf = k;
-    else if(prevVf-2 < 0) leftVf = k-1;
-    else leftVf = prevVf-2;
-    
-    while(rVf == leftVf){
-      rVf = floor(random(1,k+1));
-    }
-    prevVf = rVf;
-    
-    pct[0] = ((s-1)*pct[0]+xVf[rVf])/s;
-    pct[1] = ((s-1)*pct[1]+yVf[rVf])/s;
-  }
-}
 //functia shinyjs care poate fi apelata din R
 shinyjs.draw = function(params) {
   var defaultParams = {
@@ -186,8 +27,6 @@ shinyjs.draw = function(params) {
   document.getElementById("nrVfHistory").innerHTML+=params.nrVfInput+", ";
   document.getElementById("razaHistory").innerHTML+=params.razaInput+", ";
   document.getElementById("formaHistory").innerHTML+=params.formaInput+", ";
-  //shp4(3, width/2-25, 2);
-  // nu merge sh4 tre sa fac ceva conexiune cu output
   noLoop();
   
 }'
@@ -195,8 +34,6 @@ shinyjs.draw = function(params) {
 
 # aici e interfata
 ui <- fluidPage(
-    theme="style.css",
-    includeHTML("index.html"),
     useShinyjs(),
     #aici apar functiile shinyjs din javascript(singurele functii care pot fi chemate din R)
     extendShinyjs(text = jsCode, functions = c("draw","setup")),
